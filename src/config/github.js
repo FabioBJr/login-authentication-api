74fe5b259ab495803ec0c32831e0bf200b4030f9
Fabio Brito <fabiobritojunior1101@gmail.com>
2026-05-13 00:58:16 -0300

    fix: added a new config file

--- src/config/github.js
+++
@@ -1,7 +0,0 @@
-import process from 'node:process';
-import 'dotenv/config';
-
-export default {
-    clientId: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-};
